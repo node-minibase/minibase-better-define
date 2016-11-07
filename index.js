0b66dfb5d06501e339573348b7804ad956962cf2
tunnckoCore <mameto_100@mail.bg>
2016-11-07 18:46:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * minibase-better-define <https://github.com/node-minibase/minibase-better-define>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * minibase-better-define <https://github.com/node-minibase/minibase-better-define>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('mukla')
+
+test(function (done) {
+  done()
+})
