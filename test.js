e88f414430cc36508cd99cbf39db4610b5b856fe
tunnckoCore <mameto_100@mail.bg>
2016-12-05 15:56:45 +0200

    fix(verb): update layout and url links

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * minibase-better-define <https://github.com/node-minibase/minibase-better-define>
  *
- * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
  * Released under the MIT license.
  */
 
